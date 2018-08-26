4291af5091af7698065c215d57f7b140715bf682
Toby Isaac <tisaac@cc.gatech.edu>
2018-08-25 22:03:33 -0400

    slight rewording

--- utils/jupyter_notebook_job.sh
+++ utils/jupyter_notebook_job.sh
@@ -7,5 +7,5 @@ logfile=${PBS_O_WORKDIR}/${PBS_JOBNAME}.${PBS_JOBID%.ice-sched.pace.gatech.edu}.
 host=${HOSTNAME%.pace.gatech.edu}
 printf "To connect to your notebook, run the following command on your laptop/workstation:\\n\\n" > ${logfile}
 printf "ssh -t -L ${JNPORT}:localhost:${JNPORT} ${USER}@coc-ice.pace.gatech.edu ssh -L ${JNPORT}:localhost:${JNPORT} ${host}\\n\\n" >> ${logfile}
-printf "Do not close the terminal where that is running, and open the address in your browser:\\n\\n" >> ${logfile}
+printf "Do not close the terminal where that is running, and open the address below in your browser:\\n\\n" >> ${logfile}
 jupyter notebook --no-browser --port=${JNPORT} &> ${logfile}.addr
