05cc24076713de7ddeac5a1567149290915ecbf8
Toby Isaac <tisaac@cc.gatech.edu>
2018-09-11 08:27:51 -0400

    Checkpoint

--- assignments/interacting-particles/cloud.c
+++ assignments/interacting-particles/cloud.c
@@ -8,8 +8,6 @@
 #include "cloud.h"
 #include "verlet.h"
 
-#define PI 3.1415926535897932384626433832795L
-
 int safe_malloc (size_t count, void *out, const char * file, const char * fn, int line)
 {
   if (!count) {
@@ -26,69 +24,6 @@ int safe_malloc (size_t count, void *out, const char * file, const char * fn, in
 
 #define safeMALLOC(count,out) safe_malloc (count, out, __FILE__, __func__, __LINE__)
 #define CHK(Q) if (Q) return Q
-
-static void
-initialize_variables (int Np, double k, cse6230rand_t *rand, double *X0[3], double *X[3], double *U[3])
-{
-  size_t init_tag;
-
-  init_tag = cse6230rand_get_tag (rand);
-  for (int i = 0; i < Np; i+=4) { /* for every particle */
-    double xval[3][4];
-    double uval[3][4];
-
-    for (int d = 0; d < 3; d++) { /* get four random doubles for each variable */
-      cse6230rand_hash (rand, init_tag, i,     d, 0, &xval[d][0]);
-      if (k) {
-        cse6230rand_hash (rand, init_tag, i, 3 + d, 0, &uval[d][0]);
-      }
-      else {
-        for (int j = 0; j < 4; j++) {
-          uval[d][j] = 0.5;
-        }
-      }
-    }
-
-    if (i + 4 <= Np) {
-      for (int j = 0; j < 4; j++) {
-        for (int d = 0; d < 3; d++) { /* scale uniform [0,1) variables to [-1, 1) */
-          X0[d][i + j] = X[d][i + j] = 2. * xval[d][j] - 1.;
-          U[d][i + j] = 2. * uval[d][j] - 1.;
-        }
-      }
-    }
-    else {
-      for (int j = 0; j < Np - i; j++) {
-        for (int d = 0; d < 3; d++) {
-          X0[d][i + j] = X[d][i + j] = 2. * xval[d][j] - 1.;
-          U[d][i + j] = 2. * uval[d][j] - 1.;
-        }
-      }
-    }
-  }
-}
-
-static double
-compute_hamiltonian (int Np, double k,
-                     const double *X[3],
-                     const double *U[3])
-{
-  double h = 0.;
-
-  for (int i = 0; i < Np; i++) { /* for every particle */
-    h += 0.5 * (U[0][i]*U[0][i] + U[1][i]*U[1][i] + U[2][i]*U[2][i]); /* kinetic energy */
-    if (k) {
-      for (int j = i + 1; j < Np; j++) { /* for every other particle */
-        if (j != i) {
-          double hj = potential (k, X[0][i], X[1][i], X[2][i], X[0][j], X[1][j], X[2][j]);
-
-          h += hj;
-        }
-      }
-    }
-  }
-  return h;
-}
 
 static int
 write_step (int Np, double T, double H, const double *X[3])
@@ -208,8 +143,8 @@ main (int argc, char **argv)
     avgDist /= Np;
 
     if (!gifname) {
-      printf ("[%s] Simulation time: %g\n", argv[0], loop_time);
-      printf ("[%s] Hamiltonian, T = %g: %g, Relative Error: %g\n", argv[0], Nt * dt, Hout, fabs(Hout - Hin) / Hin);
+      printf ("[%s] Simulation walltime: %g\n", argv[0], loop_time);
+      printf ("[%s] Hamiltonian, T = %g: %g, Relative Error: %g\n", argv[0], Nt * dt, Hout, Hin ? fabs(Hout - Hin) / Hin: 0.);
       printf ("[%s] Average Distance Traveled: %g\n", argv[0], avgDist);
     } else {
       printf ("{ \"avg_dist\": %e, \"hamiltonian_T\": %e, \"hamiltionian_relerr\": %e, \"sim_time\": %e }\n",
--- assignments/interacting-particles/cloud.h
+++ assignments/interacting-particles/cloud.h
@@ -66,4 +66,7 @@ force (double k,
   f[2] = k * ir3 * qdz * dqdist(dz);
 }
 
+void initialize_variables (int Np, double k, cse6230rand_t *rand, double *X0[3], double *X[3], double *U[3]);
+double compute_hamiltonian (int Np, double k, const double *X[3], const double *U[3]);
+
 #endif
