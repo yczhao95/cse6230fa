1b030f2838146367f5948d68df0a1ec987182566
Yincheng Zhao <yzhao492@rich133-h35-16-r.pace.gatech.edu>
2018-12-16 16:54:49 -0500

    interacting particles

--- assignments/interacting-particles/verlet_accelerate.c
+++ assignments/interacting-particles/verlet_accelerate.c
@@ -5,23 +5,32 @@
 void
 verlet_step_accelerate (int Np, double dt, const double *restrict X[3], double *restrict U[3])
 {
-  for (int i = 0; i < Np; i++) {
-    double u[3] = {0.};
+  #pragma omp parallel
+	{
+	double U_private[3][Np];
+	#pragma omp for
+	for (int i = 0; i < Np; i++) {
+		double u1 = 0., u2 = 0., u3 = 0.;
+		for (int j = 0; j < Np; j++) {
+			if (j != i) {
+				double du[3];
+				force (dt, X[0][i], X[1][i], X[2][i], X[0][j], X[1][j], X[2][j], du);
 
-    for (int j = 0; j < Np; j++) {
-      if (j != i) {
-        double du[3];
-
-        force (dt, X[0][i], X[1][i], X[2][i], X[0][j], X[1][j], X[2][j], du);
-
-        for (int d = 0; d < 3; d++) {
-          u[d] += du[d];
-        }
-      }
-    }
-    for (int d = 0; d < 3; d++) {
-      U[d][i] += u[d];
-    }
-  }
+					u1 += du[0];
+					u2 += du[1];
+					u3 += du[2];
+			} 
+		}
+		U_private[0][i] += u1;
+		U_private[1][i] += u2;
+		U_private[2][i] += u3;
+	}
+  #pragma omp critical
+	for (int i = 0; i < Np; i++) {
+		for (int d = 0; d < 3; d++) {
+			U[d][i] += U_private[d][i];
+		}
+	}
+	}
 }
 
--- assignments/interacting-particles/verlet_accelerate2.c
+++ assignments/interacting-particles/verlet_accelerate2.c
@@ -5,17 +5,26 @@
 void
 verlet_step_accelerate (int Np, double dt, const double *restrict X[3], double *restrict U[3])
 {
-  for (int i = 0; i < Np; i++) {
-    for (int j = i + 1; j < Np; j++) {
-      double du[3];
+		#pragma omp parallel
+		{	
+		double U_private[3][Np];
+		#pragma omp for
+		for (int i = 0; i < Np; i++) {
+			for (int j = i + 1; j < Np; j++) {
+				double du[3];
+				force (dt, X[0][i], X[1][i], X[2][i], X[0][j], X[1][j], X[2][j], du);
+				for (int d = 0; d < 3; d++) {
+					U_private[d][i] += du[d];
+					U_private[d][j] -= du[d];
+				}
+			}
+		}
+    #pragma omp critical
+		for (int i = 0; i < Np; i++) {
+			for (int d = 0; d < 3; d++) {
+				U[d][i] += U_private[d][i];
+			}
+		}
+		}
 
-      force (dt, X[0][i], X[1][i], X[2][i], X[0][j], X[1][j], X[2][j], du);
-
-      for (int d = 0; d < 3; d++) {
-        U[d][i] += du[d];
-        U[d][j] -= du[d];
-      }
-    }
-  }
 }
-
